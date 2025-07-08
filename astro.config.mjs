5c185fdcafdd7cbfd9cb9fd30d1447c977265f85
Nicolas Durand <nicolas.p.durand@icloud.com>
2025-07-08 16:39:12 +0200

    update conf astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://lynkhirosue.github.io',
-  base: '/edb-website', 
+  site: 'https://lecoledubelier.beer',
 });
