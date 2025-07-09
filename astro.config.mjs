465f54d20d28b36ae225734145808927282d599a
Nicolas Durand <nicolas.p.durand@icloud.com>
2025-07-09 15:53:32 +0200

    fix tailwind. refacto. tout ça...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lecoledubelier.beer',
-  integrations: [tailwind()],
 
   image: {
     domains: ['lecoledubelier.beer'],
