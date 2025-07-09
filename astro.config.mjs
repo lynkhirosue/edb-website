1ac14d6f814a6749c85b9c894b43611c0ca569f3
Nicolas Durand <nicolas.p.durand@icloud.com>
2025-07-09 15:35:42 +0200

    adding tailwind. Refactoring.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://lecoledubelier.beer',
+  integrations: [tailwind()],
+
+  image: {
+    domains: ['lecoledubelier.beer'],
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
---
+++ tailwind.config.mjs
@@ -0,0 +1,14 @@
+export default {
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        amber: { 600: '#D2691E' },
+        cuivre: '#B87333',
+      },
+      fontFamily: {
+        serif: ['Merriweather', 'serif'],
+      },
+    },
+  },
+};
